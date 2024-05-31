7fb6fefde3a59853baa29a484ac08c71f6d357b0
Marlon Martins <marlon-martins@outlook.com>
2024-06-01 01:03:29 +0200

    Project Folder Structure

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// Components
+import Navbar from './sections/navbar/Navbar';
+import Header from './sections/navbar/Header';
+import About from './sections/navbar/About';
+
+
+const App = () => {
+  return (
+    <main>
+      <Navbar/>
+      <Header/>
+      <About/>
+    </main>
+  )
+}
+
+export default App;
