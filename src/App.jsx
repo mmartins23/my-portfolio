42bb3eb22a334e9c4e8972936a6a0f8dddb8a493
Marlon Martins <marlon-martins@outlook.com>
2024-06-18 13:34:05 +0200

    Project Folder Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 // Components
 import Navbar from './sections/navbar/Navbar';
-import Header from './sections/navbar/Header';
-import About from './sections/navbar/About';
+import Header from './sections/header/Header';
+import About from './sections/about/About';
+import Services from './sections/services/Services';
+import Portfolio from './sections/portfolio/Portfolio';
+import Contact from './sections/contact/Contact';
+import Footer from './sections/footer/Footer';
+import FloatingNav from './sections/floating-nav/FloatingNav';
 
 
 const App = () => {
@@ -10,6 +15,11 @@ const App = () => {
       <Navbar/>
       <Header/>
       <About/>
+      <Services/>
+      <Portfolio/>
+      <Contact/>
+      <Footer/>
+      <FloatingNav/>
     </main>
   )
 }
