ca6a8114dc6efe7a3e0dfe34c9b319455d923e99
Marlon Martins <marlon-martins@outlook.com>
2024-06-18 17:22:28 +0200

    Navbar CSS

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,24 +1,22 @@
 import Logo from '../../assets/logo.jpeg';
 import data from './data';
-import './navbar.css'
+import './navbar.css';
 
 const Navbar = () => {
   return (
-    <nav>
-      <div>
-        <a href="index.html" className='nav__logo'>
+    <nav className="nav">
+      <div className="container nav__container">
+        <a href="index.html" className="nav__logo">
           <img src={Logo} alt="Logo" />
         </a>
-        <ul className='nav__menu'>
-          {
-            data.map(item => (
-              <li key={item.id}><a href={item.link}>{item.title}</a></li>
-            ))
-          }
+        <ul className="nav__menu">
+          {data.map(item => (
+            <li key={item.id}><a href={item.link}>{item.title}</a></li>
+          ))}
         </ul>
       </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
