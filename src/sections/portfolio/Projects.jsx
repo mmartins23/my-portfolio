ab8d8e97453d4cc4b112529afc027d98d389d214
Marlon Martins <marlon-martins@outlook.com>
2024-06-25 15:20:15 +0200

    Projects Component

--- src/sections/portfolio/Projects.jsx
+++ src/sections/portfolio/Projects.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import Project from "./Project"
 
-const Projects = () => {
+const Projects = ({ projects }) => {
   return (
-    <div>Projects</div>
+    <div className="portfolio-projects">
+      {
+        projects.map(project => (
+          <Project key={project.id} project={project} />
+        ))
+      }
+    </div>
   )
 }
 
